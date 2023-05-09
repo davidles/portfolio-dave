46f68a439da3e6ffc69500b5f95837cdebf916ce
david lescano <lescanodavid07@gmail.com>
2023-05-09 09:54:37 -0300

    created project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react';
+import './App.css';
+
+// or less ideally
+import { Button } from 'react-bootstrap';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Button>Click</Button>
+    </>
+  )
+}
+
+export default App
