93e99f6df2d9f46df0df1db263ca315f9e1524e3
david lescano <lescanodavid07@gmail.com>
2023-05-14 12:09:05 -0300

    Add styles to the banner component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,3 @@
-
-import Button from 'react-bootstrap/Button';
 import perfil from '../../assets/FotoPerfil.jpeg';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
@@ -9,18 +7,21 @@ import Row from 'react-bootstrap/Row';
 export default function Banner() {
 
     return (
-        <Container style={{width: '80%'}}>
-            <Row style={{alignContent: "center", justifyContent: 'center'}}>
-                <Col xs={6} md={6} style={{textAlign: "center"}}>
-                    <h1>Frontend developer & Mentor.</h1>
-                    <p>
+        <Container className='banner__container'>
+            <Row className='banner__container--row'>
+                <Col md={12} className='row__col'>
+                    <h1 className='row__col--title'>Frontend developer & Mentor.</h1>
+                    <p className='row__col--p'>
                         I design and code beautifully simple things.
                     </p>
                     <Image
                         src={perfil}
-                        style={{height: "15em", width: "16em"}}
+                        className='row__col--img'
                         roundedCircle
                     />
+                    <p className='row__col--p'>
+                        Hey there! Want to take your online presence to the next level? Let me help you create compelling app that converts and leaves a lasting impression on your audience.
+                    </p>
                 </Col>
             </Row>
         </Container>
