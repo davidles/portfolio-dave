38f88772b5ae3a37b2e9c82a0baa4ffa7b3f056a
david lescano <lescanodavid07@gmail.com>
2023-05-14 20:26:08 -0300

    Home done

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@
 import './App.css';
 import Banner from './components/Banner/Banner';
 
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
 function App() {
   return (
-    <>
-      <Banner/>
-    </>
+    <Container fluid >
+
+      <Row >
+        <Col md={7}>
+          <div className='animate-cards'></div>
+        </Col>
+        <Col md={5}>
+          <Banner />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
 export default App
+
+
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,13 +1,44 @@
-import perfil from '../../assets/FotoPerfil.jpeg';
+import { useState } from 'react';
+
+import perfil from '../../assets/img/FotoPerfil.jpeg';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
 
 export default function Banner() {
 
+    const [turn, setTurn] = useState(false);
+
+    const handleTurn = () => {
+        console.log({ turn })
+        setTurn(!turn)
+    }
+
+    if (turn) {
+        return (
+            <Container className={`banner__container ${turn ? 'giro' : ''}`} style={{ background: 'white' }} >
+                <Row className='banner__container--row'>
+                    <Col md={12} className='row__col'>
+                        <h2>ASD</h2>
+                        <Button
+                            variant="info"
+                            size="lg"
+                            className='row__col--button'
+                            onClick={handleTurn}
+                        >
+                            Let's Go
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+
     return (
-        <Container className='banner__container'>
+        <Container className={`banner__container ${turn ? 'giro' : ''}`}>
             <Row className='banner__container--row'>
                 <Col md={12} className='row__col'>
                     <h1 className='row__col--title'>Frontend developer & Mentor.</h1>
@@ -17,11 +48,21 @@ export default function Banner() {
                     <Image
                         src={perfil}
                         className='row__col--img'
-                        roundedCircle
+                        roundedCircle 
                     />
+
+
                     <p className='row__col--p'>
                         Hey there! Want to take your online presence to the next level? Let me help you create compelling app that converts and leaves a lasting impression on your audience.
                     </p>
+                    <Button
+                        variant="info"
+                        size="lg"
+                        className='row__col--button'
+                        onClick={handleTurn}
+                    >
+                        Let's Go
+                    </Button>
                 </Col>
             </Row>
         </Container>
